f5551da28c79444969b62b85871ab170d3620eed
Murali <67466723+codearbor@users.noreply.github.com>
2021-06-26 16:35:04 +0530

    Project Setup

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.ts',
+  },
+  output: {
+    filename: 'data-ajax.min.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  target: ['web', 'es5'],
+  externals: {
+    // require("jquery") is external and available
+    //  on the global var jQuery
+    "jquery": "jQuery"
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false,
+            max_line_len: 500
+          }
+        }
+      }),
+    ],
+  },
+  performance: {
+    maxEntrypointSize: 400000,
+  },
+  stats: {
+    children: true
+  },
+}
